6f44030e4954bc173985dd71ee78db7acf392c05
Ana Beatriz <anabiawalker@gmail.com>
2022-01-18 14:26:19 -0300

    Implementando as rotas

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -5,8 +5,8 @@ import NavItem from './NavItem'
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            <NavItem path="#/" icon="home" title="Início"/>
+            <NavItem path="/" icon="home" title="Início"/>
 
-            <NavItem path="#/users" icon="users" title="Usuários"/>
+            <NavItem path="/users" icon="users" title="Usuários"/>
         </nav>
     </aside>
