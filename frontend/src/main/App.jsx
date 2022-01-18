ce8697889e8ed03903c03a282fff2773c693595f
Ana Beatriz <anabiawalker@gmail.com>
2022-01-18 13:56:17 -0300

    Adicionando Componente Home

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,18 +5,14 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
     <div className="app">
         <Logo/>
         <Nav/>
-        <Main icon="home" title="Início" subtitle="Segundo projeto do Capítulo de React 16.4">
-            <div className="display-4">Bem-vindo!</div>
-            <hr />
-            <p className='mb-0'>Sistema para exemplificar a construção de um cadastro de usuários desenvolvido em React.</p>
-        </Main>
+        <Home/>
         <Footer/>
     </div>
 
