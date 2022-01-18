0fe40608bbdb7d2a5215ec97514b76181ca276c0
Ana Beatriz <anabiawalker@gmail.com>
2022-01-18 13:35:35 -0300

    Estilizando Componente Footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Feito por <strong>Ana Beatriz da Conceição Walker</strong>. Desenvolvido com <i className='fa fa-heart text-danger'></i> por <strong>Cod<span className='text-danger'>3</span>r Cursos Online</strong>
+        </span>
     </footer>
