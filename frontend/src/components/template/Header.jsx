98e5c0eb97b97e7e0194cec111bc9c65b32ef446
Ana Beatriz <anabiawalker@gmail.com>
2022-01-18 13:28:04 -0300

    Estilizando Componente Header

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -2,6 +2,9 @@ import './Header.css'
 import React from 'react'
 
 export default props => 
-    <header className="header">
-        Cabeçalho
+    <header className="header d-none d-sm-flex flex-column"> {/*Responsividade*/}
+        <h1 className="mt-3"> {/*margin-top: 3px*/}
+            <i className={`fa fa-${props.icon}`}></i> {props.title}
+        </h1>
+        <p className='lead text-muted'>{props.subtitle}</p>
     </header>
--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -4,7 +4,7 @@ import Header from './Header'
 
 export default props =>
     <React.Fragment>
-        <Header/>
+        <Header {...props}/> {/*Recebe as propriedades passadas em App.jsx*/}
         <main className='content'>
             Conteúdo
         </main>
